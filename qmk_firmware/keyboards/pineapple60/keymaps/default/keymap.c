b1282beec1bcbbf37d967005c2ae26167b844e03
saoto tsuchiya <saoto.tsuchiya.oc@futu-re.co.jp>
2021-09-20 21:04:27 +0900

    update qmk/keymaps

--- qmk_firmware/keyboards/pineapple60/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/pineapple60/keymaps/default/keymap.c
@@ -43,12 +43,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------'     `---------------------------| Left | Down | Right|
    */
     [_BASE] = LAYOUT(
-      KC_ESC,   KC_1,    KC_2,      KC_3,     KC_4,            KC_5,     KC_6,   KC_7,           KC_8,    KC_9,    KC_0,     KC_MINS, KC_DEL,
-      KC_TAB,   KC_Q,    KC_W,      KC_E,     KC_R,            KC_T,     KC_Y,   KC_U,           KC_I,    KC_O,    KC_P,     KC_MINS, KC_BSPC,
-      KC_LSFT,  KC_A,    KC_S,      KC_D,     KC_F,            KC_G,     KC_H,   KC_J,           KC_K,    KC_L,    KC_SCOLON,KC_QUOT, KC_ENT,
-      KC_LCTL,  KC_Z,    KC_X,      KC_C,     KC_V,            KC_B,     KC_N,   KC_M,           KC_COMM, KC_DOT,  KC_SLSH,  KC_BSLS, KC_RSFT,
-      KC_CAPS,  KC_LGUI, A(KC_LEFT), KC_LALT, LT(_NUM,KC_MHEN),KC_SPC,   KC_SPC,LT(_FNC,KC_HENK),KC_RALT, KC_RCTL, KC_P9,    KC_UP,   KC_P3,
-                                                                    KC_ESC,                                        KC_LEFT,  KC_DOWN, KC_RGHT
+      KC_ESC,   KC_1,    KC_2,      KC_3,     KC_4,      KC_5,     KC_6,   KC_7,     KC_8,    KC_9,    KC_0,     KC_MINS, KC_DEL,
+      KC_TAB,   KC_Q,    KC_W,      KC_E,     KC_R,      KC_T,     KC_Y,   KC_U,     KC_I,    KC_O,    KC_P,     KC_MINS, KC_BSPC,
+      KC_LSFT,  KC_A,    KC_S,      KC_D,     KC_F,      KC_G,     KC_H,   KC_J,     KC_K,    KC_L,    KC_SCOLON,KC_QUOT, KC_ENT,
+      KC_LCTL,  KC_Z,    KC_X,      KC_C,     KC_V,      KC_B,     KC_N,   KC_M,     KC_COMM, KC_DOT,  KC_SLSH,  KC_BSLS, KC_RSFT,
+      KC_CAPS,  KC_LGUI, A(KC_LEFT), KC_LALT, TT(_NUM),  KC_SPC,   KC_SPC, TT(_FNC), KC_RALT, KC_RCTL, KC_PGUP,  KC_UP, KC_PGDN,
+                                                              KC_ESC,                                  KC_LEFT,  KC_DOWN, KC_RGHT
     ),
  /* Fnc
    * ,-----------------------------------------.     ,------------------------------------------------.
@@ -66,8 +66,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_FNC] = LAYOUT(
       _______, KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,      KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,
       KC_ESC,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,      KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_PDOT,  _______,
-      _______, _______,  KC_PSCR,  _______,  _______, KC_HOME,  KC_PGUP,    KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_INS,   _______,
-      _______, KC_MUTE,  KC_VOLD,  KC_VOLU,  _______, KC_END,   KC_PGDN,    _______,  KC_BRID,  KC_BRIU,  KC_WHOM,  _______,  _______,
+      _______, _______,  KC_PSCR,  _______,  _______, KC_HOME,    KC_PGUP,    KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_INS,   _______,
+      _______, KC_MUTE,  KC_VOLD,  KC_VOLU,  _______, KC_END,     KC_PGDN,    _______,  KC_BRID,  KC_BRIU,  KC_WHOM,  _______,  _______,
       _______, _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,
                                                              _______,                                      _______,  _______,  _______
     ),
--- qmk_firmware/keyboards/pineapple60/keymaps/jis/keymap.c
+++ qmk_firmware/keyboards/pineapple60/keymaps/jis/keymap.c
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,   KC_Q,    KC_W,      KC_E,     KC_R,            KC_T,     KC_Y,   KC_U,           KC_I,    KC_O,    KC_P,     KC_MINS, KC_BSPC,
       KC_LSFT,  KC_A,    KC_S,      KC_D,     KC_F,            KC_G,     KC_H,   KC_J,           KC_K,    KC_L,    KC_SCOLON,KC_QUOT, KC_ENT,
       KC_LCTL,  KC_Z, KC_X,      KC_C,     KC_V,            KC_B,     KC_N,   KC_M,           KC_COMM, KC_DOT,  KC_SLSH,  KC_RO,   KC_RSFT,
-      KC_CAPS,  KC_LGUI, A(KC_LEFT), KC_LALT, LT(_NUM,KC_MHEN),KC_SPC,   KC_SPC,LT(_FNC,KC_HENK),KC_RALT, KC_RCTL, KC_P9,    KC_UP,   KC_P3,
+      KC_CAPS,  KC_LGUI, A(KC_LEFT), KC_LALT, LT(_NUM,KC_MHEN),KC_SPC,   KC_SPC,LT(_FNC,KC_HENK),KC_RALT, KC_RCTL, KC_PGUP,    KC_UP,   KC_PGDN,
                                                                     KC_ESC,                                        KC_LEFT,  KC_DOWN, KC_RGHT
     ),
  /* Fnc
